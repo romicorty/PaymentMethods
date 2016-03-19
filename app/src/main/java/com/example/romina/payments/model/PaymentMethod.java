8a690d5486686c4e78b68a12db8f9c463c6a97df
Romina <romicorty@gmail.com>
2016-03-19 16:03:10 -0300

    Se agrega el modelo

---
+++ app/src/main/java/com/example/romina/payments/model/PaymentMethod.java
@@ -0,0 +1,32 @@
+package com.example.romina.payments.model;
+
+
+public class PaymentMethod {
+    private String id;
+    private String name;
+    private String payment_type_id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPayment_type_id() {
+        return payment_type_id;
+    }
+
+    public void setPayment_type_id(String payment_type_id) {
+        this.payment_type_id = payment_type_id;
+    }
+}
