e9a53cea00b1612248b05743b3de3fe9c3915017
Romina <romicorty@gmail.com>
2016-03-19 19:14:29 -0300

    Se agrega listado

---
+++ app/src/main/java/com/example/romina/payments/network/PaymentMethodAdapter.java
@@ -0,0 +1,54 @@
+package com.example.romina.payments.network;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.example.romina.payments.R;
+import com.example.romina.payments.model.PaymentMethod;
+import java.util.List;
+
+public class PaymentMethodAdapter extends ArrayAdapter<PaymentMethod> {
+
+    private List<PaymentMethod> mPaymentMethods;
+    private int mResourceId;
+
+    public PaymentMethodAdapter(Context context, int resource, List<PaymentMethod> paymentMethods) {
+        super(context, resource, paymentMethods);
+        mPaymentMethods = paymentMethods;
+        mResourceId = resource;
+    }
+
+    public static class PaymentMethodViewHolder {
+        TextView txName;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        PaymentMethodViewHolder viewHolder;
+
+        if(convertView == null){
+            viewHolder = new PaymentMethodViewHolder();
+            LayoutInflater li = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = li.inflate(mResourceId,parent,false);
+            viewHolder.txName = (TextView)convertView.findViewById(R.id.txName);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (PaymentMethodViewHolder)convertView.getTag();
+        }
+
+        PaymentMethod paymentMethod = mPaymentMethods.get(position);
+        viewHolder.txName.setText(paymentMethod.getName());
+
+        return convertView;
+    }
+
+
+    public void updatePaymentMethods(List<PaymentMethod> payments){
+        mPaymentMethods.clear();
+        mPaymentMethods.addAll(payments);
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/example/romina/payments/network/PaymentMethodListActivity.java
+++ app/src/main/java/com/example/romina/payments/network/PaymentMethodListActivity.java
@@ -4,15 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.example.romina.payments.R;
+import com.example.romina.payments.model.PaymentMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PaymentMethodListActivity extends AppCompatActivity {
+    private PaymentMethodAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_method_list);
+        List<PaymentMethod> payments = new ArrayList<>();
+        mAdapter = new PaymentMethodAdapter(this,R.layout.payment_method_item,payments);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(mAdapter);
+        loadPaymentMethods();
     }
 
     @Override
@@ -35,5 +46,16 @@ public class PaymentMethodListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void loadPaymentMethods(){
+        List<PaymentMethod> list = new ArrayList<>();
+        PaymentMethod p1 = new PaymentMethod();
+        p1.setName("Visa");
+        list.add(p1);
+        PaymentMethod p2 = new PaymentMethod();
+        p2.setName("American Express");
+        list.add(p2);
+        mAdapter.updatePaymentMethods(list);
     }
 }
