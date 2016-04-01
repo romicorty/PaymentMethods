433ccd2824df97db7775e64aabcf3066fbeaf0da
Romina <romicorty@gmail.com>
2016-04-01 00:13:35 -0300

    Se crea fragment para la carga del importe

---
+++ app/src/main/java/com/example/romina/payments/AmountFragment.java
@@ -0,0 +1,67 @@
+package com.example.romina.payments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.math.BigDecimal;
+
+
+public class AmountFragment extends Fragment {
+
+    private AmountFragmentListener mListener;
+
+    public AmountFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_amount, container, false);
+        Button btn = (Button) view.findViewById(R.id.btn_next);
+        final EditText txAmount = (EditText)view.findViewById(R.id.tx_amount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    BigDecimal amount = new BigDecimal(txAmount.getText().toString());
+                    mListener.onInputAmount(amount);
+                }
+            }
+        });
+
+        return view;
+    }
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (AmountFragmentListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement AmountFragmentListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface AmountFragmentListener {
+        public void onInputAmount(BigDecimal amount);
+    }
+
+}
